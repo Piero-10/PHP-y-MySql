d7b73294232d6fa567acbfadfda8606453bdb58e
Piero-10 <38970746+Piero-10@users.noreply.github.com>
2018-05-10 01:04:43 -0500

    Principal
    vista principal de mi sistema de publicaciones

---
+++ clase-04-07-may-2018-index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Mi Sistema de Publicaciones</title>
+  </head>
+  <body>
+    <div style="text-align:center;" >
+      Mi sistema de publicacio es
+    </div>
+    <div style="text-align:center;">
+      Inicio | Nuevo Editor | Nueva Noticia | Editar Noticia |
+    </div>
+
+  </body>
+</html>
