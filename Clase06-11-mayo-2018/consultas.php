5cf7adf252cdb7e55278b7f9bc05971f40e46e88
Piero-10 <38970746+Piero-10@users.noreply.github.com>
2018-05-17 11:53:40 -0500

    Create consultas.php

---
+++ Clase06-11-mayo-2018/consultas.php
@@ -0,0 +1,46 @@
+<?php
+//Autor: Claudio Morales
+//Fecha: 11 mayo 2018
+//Video del Webinar: 
+define("SERVER",      "localhost");
+define("USUARIO",     "root");
+define("PASS",        "");
+define("NOMBREBASE",  "nuevabase");
+class ClaseConsultasBase
+  {
+      private $objetoBase = '';
+      public function __construct()
+        {
+            $this->objetoBase = new mysqli(SERVER, USUARIO, PASS, NOMBREBASE);
+            if($this->objetoBase->connect_errno)
+              {
+                  echo "Fallo la conexion: ".$objetoMysqli->connect_error;
+              }
+        }
+      public function listarNoticias()
+        {
+            $query = "SELECT * FROM publicacion";
+            $b='';
+            if($mistados = $this->objetoBase->query($query)) 
+              {
+                  while ($a = $mistados->fetch_assoc()) 
+                  {
+                     $b[] = $a;
+                  }
+              }
+            return $b;
+        }
+      public function detalleNoticia()
+        {
+          $id = $_GET["id"];
+          $query = 
+          "SELECT email_autor, fechahora, titulo, textopublicacion
+          FROM publicacion where id = $id ";
+          $a = '';
+          if($result = $this->objetoBase->query($query)) 
+              {
+                 $a = $result->fetch_assoc();
+              }
+          return $a;
+        }
+  }
